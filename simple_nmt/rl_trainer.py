d6a7cd2839f4361818d945a6835d466d061a782b
Ki Hyun Kim <pointzz.ki@gmail.com>
2018-06-14 09:16:38 +0900

    add rl training

---
+++ simple_nmt/rl_trainer.py
@@ -0,0 +1,220 @@
+import time
+import numpy as np
+#from nltk.translate.bleu_score import sentence_bleu as score_func
+from nltk.translate.gleu_score import sentence_gleu as score_func
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.utils as torch_utils
+
+import utils
+import data_loader
+
+def get_reward(y, y_hat):
+    # |y| = (batch_size, length1)
+    # |y_hat| = (batch_size, length2)
+
+    scores = []
+
+    for b in range(y.size(0)):
+        ref = []
+        hyp = []
+        for t in range(y.size(1)):
+            ref += [str(int(y[b, t]))]
+            if y[b, t] == data_loader.EOS:
+                break
+
+        for t in range(y_hat.size(1)):
+            hyp += [str(int(y_hat[b, t]))]
+            if y_hat[b, t] == data_loader.EOS:
+                break
+
+        scores += [score_func([ref], hyp) * 100.]
+    scores = torch.FloatTensor(scores).to(y.device)
+    # |scores| = (batch_size)
+
+    return scores
+
+def get_gradient(y, y_hat, criterion, reward = 1):
+    # |y| = (batch_size, length)
+    # |y_hat| = (batch_size, length, output_size)
+    # |reward| = (batch_size)
+    batch_size = y.size(0)
+
+    # Before we get the gradient, multiply -reward for each sample and each time-step.
+    y_hat = y_hat * -reward.view(-1, 1, 1).expand(*y_hat.size())
+
+    log_prob = -criterion(y_hat.contiguous().view(-1, y_hat.size(-1)), y.contiguous().view(-1))
+    log_prob.div(batch_size).backward()
+
+    return log_prob
+
+def train_epoch(model, criterion, train_iter, valid_iter, config, others_to_save = None):
+    current_lr = config.rl_lr
+
+    highest_valid_bleu = -np.inf
+    no_improve_cnt = 0
+
+    # Print initial valid BLEU before we start RL.
+    model.eval()
+    total_reward, sample_cnt = 0, 0
+    for batch_index, batch in enumerate(valid_iter):
+        current_batch_word_cnt = torch.sum(batch.tgt[1])
+        x = batch.src
+        y = batch.tgt[0][:, 1:]
+        batch_size = y.size(0)
+        # |x| = (batch_size, length)
+        # |y| = (batch_size, length)
+
+        # feed-forward
+        y_hat, indice = model.search(x, is_greedy = True, max_length = config.max_length)
+        # |y_hat| = (batch_size, length, output_size)
+        # |indice| = (batch_size, length)
+
+        reward = get_reward(y, indice)
+
+        total_reward += float(reward.sum())
+        sample_cnt += batch_size
+        if sample_cnt >= len(valid_iter.dataset.examples):
+            break
+    avg_bleu = total_reward / sample_cnt
+    print("initial valid BLEU: %.4f" % avg_bleu)
+    model.train()
+
+    # Start RL
+    for epoch in range(1, config.rl_n_epochs + 1):
+        #optimizer = optim.Adam(model.parameters(), lr = current_lr)
+        optimizer = optim.SGD(model.parameters(), lr = current_lr)
+        print("current learning rate: %f" % current_lr)
+        print(optimizer)
+
+        sample_cnt = 0
+        total_loss, total_bleu, total_sample_count, total_word_count, total_parameter_norm, total_grad_norm = 0, 0, 0, 0, 0, 0
+        start_time = time.time()
+        train_bleu = np.inf
+
+        for batch_index, batch in enumerate(train_iter):
+            optimizer.zero_grad()
+
+            current_batch_word_cnt = torch.sum(batch.tgt[1])
+            x = batch.src
+            y = batch.tgt[0][:, 1:]
+            batch_size = y.size(0)
+            # |x| = (batch_size, length)
+            # |y| = (batch_size, length)
+
+            # feed-forward
+            y_hat, indice = model.search(x, is_greedy = False, max_length = config.max_length)
+            q_actor = get_reward(y, indice)
+            # |y_hat| = (batch_size, length, output_size)
+            # |indice| = (batch_size, length)
+            # |q_actor| = (batch_size)
+
+            baseline = []
+            with torch.no_grad():
+                for i in range(config.n_samples):
+                    _, sampled_indice = model.search(x, is_greedy = False, max_length = config.max_length)
+                    baseline += [get_reward(y, sampled_indice)]
+                baseline = torch.stack(baseline).sum(dim = 0).div(config.n_samples)
+                # |baseline| = (n_samples, batch_size) --> (batch_size)
+
+            # calcuate gradients with back-propagation
+            tmp_reward = q_actor - baseline
+            # |tmp_reward| = (batch_size)
+            get_gradient(indice, y_hat, criterion, reward = tmp_reward)
+
+            # simple math to show stats
+            total_loss += float(tmp_reward.sum())
+            total_bleu += float(q_actor.sum())
+            total_sample_count += batch_size
+            total_word_count += int(current_batch_word_cnt)
+            total_parameter_norm += float(utils.get_parameter_norm(model.parameters()))
+            total_grad_norm += float(utils.get_grad_norm(model.parameters()))
+
+            if (batch_index + 1) % config.print_every == 0:
+                avg_loss = total_loss / total_sample_count
+                avg_bleu = total_bleu / total_sample_count
+                avg_parameter_norm = total_parameter_norm / config.print_every
+                avg_grad_norm = total_grad_norm / config.print_every
+                elapsed_time = time.time() - start_time
+
+                print("epoch: %d batch: %d/%d\t|param|: %.2f\t|g_param|: %.2f\trwd: %.4f\tBLEU: %.4f\t%5d words/s %3d secs" % (epoch, 
+                                                                                                            batch_index + 1, 
+                                                                                                            int(len(train_iter.dataset.examples) // config.batch_size), 
+                                                                                                            avg_parameter_norm, 
+                                                                                                            avg_grad_norm, 
+                                                                                                            avg_loss,
+                                                                                                            avg_bleu,
+                                                                                                            total_word_count // elapsed_time,
+                                                                                                            elapsed_time
+                                                                                                            ))
+
+                total_loss, total_word_count, total_parameter_norm, total_grad_norm = 0, 0, 0, 0
+                start_time = time.time()
+
+                train_bleu = avg_bleu
+
+            # Another important line in this method.
+            # In orther to avoid gradient exploding, we apply gradient clipping.
+            torch_utils.clip_grad_norm_(model.parameters(), config.max_grad_norm)
+            # Take a step of gradient descent.
+            optimizer.step()
+
+            sample_cnt += batch_size
+            if sample_cnt >= len(train_iter.dataset.examples) * .01:
+                break
+
+        sample_cnt = 0
+        total_reward = 0
+
+        with torch.no_grad():
+            model.eval()
+
+            for batch_index, batch in enumerate(valid_iter):
+                current_batch_word_cnt = torch.sum(batch.tgt[1])
+                x = batch.src
+                y = batch.tgt[0][:, 1:]
+                batch_size = y.size(0)
+                # |x| = (batch_size, length)
+                # |y| = (batch_size, length)
+
+                # feed-forward
+                y_hat, indice = model.search(x, is_greedy = True, max_length = config.max_length)
+                # |y_hat| = (batch_size, length, output_size)
+                # |indice| = (batch_size, length)
+
+                reward = get_reward(y, indice)
+
+                total_reward += float(reward.sum())
+                sample_cnt += batch_size
+                if sample_cnt >= len(valid_iter.dataset.examples):
+                    break
+
+            avg_bleu = total_reward / sample_cnt
+            print("valid BLEU: %.4f" % avg_bleu)
+
+            if highest_valid_bleu < avg_bleu:
+                highest_valid_bleu = avg_bleu
+                no_improve_cnt = 0
+            else:
+                no_improve_cnt += 1
+
+            model.train()
+
+        model_fn = config.model.split(".")
+        model_fn = model_fn[:-1] + ["%02d" % (config.n_epochs + epoch), "%.2f-%.4f" % (train_bleu, avg_bleu)] + [model_fn[-1]]
+
+        # PyTorch provides efficient method for save and load model, which uses python pickle.
+        to_save = {"model": model.state_dict(),
+                    "config": config,
+                    "epoch": epoch + 1,
+                    "current_lr": current_lr
+                    }
+        if others_to_save is not None:
+            for k, v in others_to_save.items():
+                to_save[k] = v
+        torch.save(to_save, '.'.join(model_fn))
+
+        if config.early_stop > 0 and no_improve_cnt > config.early_stop:
+            break
