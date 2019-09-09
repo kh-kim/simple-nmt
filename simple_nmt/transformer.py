97c51d61c788fc0647d6e7d12ae90d4e4beae588
Ki Hyun Kim <pointzz.ki@gmail.com>
2019-09-09 22:07:33 +0900

    add dropout

--- simple_nmt/transformer.py
+++ simple_nmt/transformer.py
@@ -80,11 +80,12 @@ class MultiHead(nn.Module):
 
 class EncoderBlock(nn.Module):
     
-    def __init__(self, hidden_size, n_splits):
+    def __init__(self, hidden_size, n_splits, dropout_p=.1):
         super().__init__()
 
         self.attn = MultiHead(hidden_size, n_splits)
-        self.norm = nn.LayerNorm(hidden_size)
+        self.attn_norm = nn.LayerNorm(hidden_size)
+        self.attn_dropout = nn.Dropout(dropout_p)
 
         self.fc = nn.Sequential(
             nn.Linear(hidden_size, hidden_size * 4),
@@ -92,27 +93,30 @@ class EncoderBlock(nn.Module):
             nn.Linear(hidden_size * 4, hidden_size),
         )
         self.fc_norm = nn.LayerNorm(hidden_size)
+        self.fc_dropout = nn.Dropout(dropout_p)
 
     def forward(self, x, mask):
         # |x| = (batch_size, n, hidden_size)
         # |mask| = (batch_size, n, n)
 
-        z = self.norm(x + self.attn(x, x, x, mask=mask))
-        z = self.fc_norm(z + self.fc(z))
+        z = self.attn_norm(x + self.attn_dropout(self.attn(x, x, x, mask=mask)))
+        z = self.fc_norm(z + self.fc_dropout(self.fc(z)))
         # |z| = (batch_size, n, hidden_size)
 
         return z, mask
 
 class DecoderBlock(nn.Module):
     
-    def __init__(self, hidden_size, n_splits):
+    def __init__(self, hidden_size, n_splits, dropout_p=.1):
         super().__init__()
 
         self.masked_attn = MultiHead(hidden_size, n_splits)
         self.masked_attn_norm = nn.LayerNorm(hidden_size)
+        self.masked_attn_dropout = nn.Dropout(dropout_p)
 
         self.attn = MultiHead(hidden_size, n_splits)
         self.attn_norm = nn.LayerNorm(hidden_size)
+        self.attn_dropout = nn.Dropout(dropout_p)
 
         self.fc = nn.Sequential(
             nn.Linear(hidden_size, hidden_size * 4),
@@ -120,35 +124,38 @@ class DecoderBlock(nn.Module):
             nn.Linear(hidden_size * 4, hidden_size),
         )
         self.fc_norm = nn.LayerNorm(hidden_size)
+        self.fc_dropout = nn.Dropout(dropout_p)
 
     def forward(self, x, KV, mask, prev):
         if prev is not None:
             # |x| = (batch_size, m=1, hidden_size)
             # |prev_i| = (batch_size, m', hidden_size)
 
-            z = self.masked_attn_norm(x + self.masked_attn(x,
-                                                           prev,
-                                                           prev,
-                                                           mask=None,
-                                                           ))
+            z = self.masked_attn_norm(x + self.masked_attn_dropout(
+                self.masked_attn(x, prev, prev, mask=None)
+            ))
             # |z| = (batch_size, 1, hidden_size)
         else:
             # |x| = (batch_size, m, hidden_size)
+            batch_size = x.size(0)
+            m = x.size(1)
 
-            forward_mask = torch.triu(torch.ones(x.size(1), x.size(1)), diagonal=1).byte().to(x.device)
+            forward_mask = torch.triu(x.new_ones((m, m)), diagonal=1).byte()
             # |forward_mask| = (m, m)
-            forward_mask = forward_mask.unsqueeze(0).expand(x.size(0), *forward_mask.size())
+            forward_mask = forward_mask.unsqueeze(0).expand(batch_size, *forward_mask.size())
             # |forward_mask| = (batch_size, m, m)
 
-            z = self.masked_attn_norm(x + self.masked_attn(x, x, x, mask=forward_mask))
+            z = self.masked_attn_norm(x + self.masked_attn_dropout(
+                self.masked_attn(x, x, x, mask=forward_mask)
+            ))
             # |z| = (batch_size, m, hidden_size)
 
         # |KV| = (batch_size, n, hidden_size)
         # |mask| = (batch_size, m, n)
-        z = self.attn_norm(z + self.attn(Q=z, K=KV, V=KV, mask=mask))
+        z = self.attn_norm(z + self.attn_dropout(self.attn(Q=z, K=KV, V=KV, mask=mask)))
         # |z| = (batch_size, m, hidden_size)
 
-        z = self.fc_norm(z + self.fc(z))
+        z = self.fc_norm(z + self.fc_dropout(self.fc(z)))
         # |z| = (batch_size, m, hidden_size)
 
         return (
@@ -177,7 +184,8 @@ class Transformer(nn.Module):
         output_size,
         n_splits,
         n_enc_blocks=6,
-        n_dec_blocks=6
+        n_dec_blocks=6,
+        dropout_p=.1,
     ):
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -185,6 +193,7 @@ class Transformer(nn.Module):
         self.n_splits = n_splits
         self.n_enc_blocks = n_enc_blocks
         self.n_dec_blocks = n_dec_blocks
+        self.dropout_p = dropout_p
 
         super().__init__()
 
@@ -192,10 +201,10 @@ class Transformer(nn.Module):
         self.emb_dec = nn.Embedding(output_size, hidden_size)
 
         self.encoder = MySequential(
-            *[EncoderBlock(hidden_size, n_splits) for _ in range(n_enc_blocks)]
+            *[EncoderBlock(hidden_size, n_splits, dropout_p) for _ in range(n_enc_blocks)]
         )
         self.decoder = MySequential(
-            *[DecoderBlock(hidden_size, n_splits) for _ in range(n_dec_blocks)],
+            *[DecoderBlock(hidden_size, n_splits, dropout_p) for _ in range(n_dec_blocks)],
         )
         self.generator = nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim=-1)
@@ -249,6 +258,7 @@ class Transformer(nn.Module):
 
     def search(self, x, is_greedy=True, max_length=255):
         # |x[0]| = (batch_size, n)
+        batch_size = x[0].size(0)
 
         mask = self._generate_mask(x[0], x[1])
         # |mask| = (batch_size, n) 
@@ -268,8 +278,7 @@ class Transformer(nn.Module):
         # |y_t_1| = (batch_size, 1)
         is_undone = x.new_ones(batch_size, 1).float()
 
-        prevs, y_hats, indice = [[] for _ in range(len(self.decoder._modules))], [], []
-
+        prevs, y_hats, indice = [[] for _ in range(len(self.decoder._modules) + 1)], [], []
         # Repeat a loop while sum of 'is_undone' flag is bigger than 0, or current time-step is smaller than maximum length.
         while is_undone.sum() > 0 and len(indice) < max_length:
             # Unlike training procedure, take the last time-step's output during the inference.
