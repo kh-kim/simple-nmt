604165875a35bfbd5e38f16e8dca415b5cbf5721
Ki Hyun Kim <pointzz.ki@gmail.com>
2020-08-23 14:13:50 +0900

    refactoring continue_*.py

--- continue_train.py
+++ continue_train.py
@@ -23,9 +23,7 @@ def overwrite_config(config, prev_config):
     return config
 
 
-if __name__ == '__main__':
-    config = define_argparser(is_continue=True)
-
+def continue_main(config, main):
     # If the model exists, load model and configuration to continue the training.
     if os.path.isfile(config.load_fn):
         saved_data = torch.load(config.load_fn, map_location='cpu' if config.gpu_id < 0 else 'cuda:%d' % config.gpu_id)
@@ -39,3 +37,8 @@ if __name__ == '__main__':
         main(config, model_weight=model_weight, opt_weight=opt_weight)
     else:
         print('Cannot find file %s' % config.load_fn)
+
+
+if __name__ == '__main__':
+    config = define_argparser(is_continue=True)
+    continue_main(config, main)
